a752e0fa5ac4bd8af266e22eda6db26db4ce991e
Fabien <fabien@tymate.com>
2019-01-09 11:11:52 +0100

    Fix rubocop offenses; Add some comments.

--- lib/generators/fetcheable_on_api/install_generator.rb
+++ lib/generators/fetcheable_on_api/install_generator.rb
@@ -1,5 +1,6 @@
 module FetcheableOnApi
   module Generators
+    # Create conf file
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __dir__)
       desc 'Creates FetcheableOnApi initializer for your application'
